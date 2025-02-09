5ee47191ecc9d77477330f76f07c25eec560cd02
John Hickey <psc.john@gmail.com>
2025-02-08 16:30:20 -0800

    fix: Update parameter name references in procedure documenter
    Fixed parameter name inconsistency in procedure documentation by updating
    references from 'parameter_name' to 'param_name' to match SQL query column
    naming in procedure_documenter.py. This fixes a KeyError that was preventing
    procedure documentation from working properly.

---
+++ src/python/services/procedure_documenter.py
@@ -0,0 +1,8 @@
+# Changed from param['parameter_name'] to param['param_name']
+param_info = {
+    'param_name': param['param_name'],
+    ...
+}
+
+# Updated debug logging
+logger.debug(f"Set documentation for parameter {param['param_name']}")
